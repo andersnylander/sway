f908b525c9b8783b7b7572af8aaa70c3d53bbe94
Anders Nylander <nylander.anders@yandex.com>
2020-04-04 16:38:36 +0200

    WIP: Add command fullscreen_present
    This command changes the scale of the output
    of the currently active container to scale the
    container output to full screen (while main-
    taining aspect ratio), before enabling full-
    screen mode.

--- include/sway/commands.h
+++ include/sway/commands.h
@@ -132,6 +132,7 @@ sway_cmd cmd_for_window;
 sway_cmd cmd_force_display_urgency_hint;
 sway_cmd cmd_force_focus_wrapping;
 sway_cmd cmd_fullscreen;
+sway_cmd cmd_fullscreen_present;
 sway_cmd cmd_gaps;
 sway_cmd cmd_hide_edge_borders;
 sway_cmd cmd_include;
--- sway/commands/fullscreen.c
+++ sway/commands/fullscreen.c
@@ -7,6 +7,58 @@
 #include "sway/tree/view.h"
 #include "sway/tree/workspace.h"
 #include "util.h"
+#include "sway/output.h"
+
+struct cmd_results *cmd_fullscreen_present(int argc, char **argv) {
+	struct sway_container *con = config->handler_context.container;
+	struct sway_workspace *ws = config->handler_context.workspace;
+	if (!con->view){
+		return cmd_results_new(CMD_FAILURE,
+			"Can't run this command if there's no view connected to the container.");
+	}
+	if (!ws->output)
+	{
+		return cmd_results_new(CMD_FAILURE,
+			"Can't run this command if no output is connected to the workspace.");
+	}
+
+	int natural_width = con->view->natural_width;
+	int natural_height = con->view->natural_height;
+	int output_width = ws->output->width;
+	int output_height = ws->output->height;
+
+
+	float scalefactor_x = output_width / (float)natural_width;
+	float scalefactor_y = output_height / (float)natural_height;
+
+	// Always pick the lower scale factor
+	float scalefactor = (scalefactor_x < scalefactor_y ? scalefactor_x : scalefactor_y);
+	char scalefactor_str[16];
+	char *output_name = ws->output->wlr_output->name;
+	char* scale_argv[] = {
+		output_name,
+		"scale",
+		scalefactor_str,
+	};
+	sprintf(scalefactor_str, "%f", scalefactor);
+
+	// Send command to change scale of output
+	struct cmd_results *result = cmd_output(3, scale_argv);
+	if (result)
+		printf("What?");
+
+	// Send command to toggle fullscreen mode for the active container
+	cmd_fullscreen(0, argv);
+
+	// Send command to center container output on the screen
+	container_set_floating(con, true);
+
+	// TODO: Store old scale and new scale somewhere (in the container?)
+	// TODO: Implement an is_valid method for old scale value
+	// TODO: Restore old scale value if it's valid after container dies
+
+	return cmd_results_new(CMD_SUCCESS, NULL);
+}
 
 // fullscreen [enable|disable|toggle] [global]
 struct cmd_results *cmd_fullscreen(int argc, char **argv) {
